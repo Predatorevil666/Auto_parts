e3bc8e2907a5c605add26c89ba051997736909c5
Alexander Batogov <predatorevil666@mail.ru>
2021-10-31 18:19:08 +0300

    2-я версия парсинга+запись в csv

--- Auto_parts.py
+++ Auto_parts.py
@@ -2,94 +2,107 @@
 
 
 import requests
-# import csv
+import csv
 from bs4 import BeautifulSoup
 
 
 
 # - Масляный фильтр hyundai solaris  (пример для ввода)
 
-def search_parts(url):    # - функция  получения ссылки фирм производителей на запрос конкретной автозапчасти
-    # r = requests.get(url)
-    # if r.ok:
+session = requests.Session()
+def load_page(url):                                                              # - функция  получения ссылки фирм производителей на запрос конкретной автозапчасти
     article = '+'.join(input('Введите название автозапчасти или ее каталожный номер с маркой автомобиля : ').split())
     link = url.format(article=article)
-    # print(link)
-    response = requests.get(link).text
-    # print(response)
+    response = session.get(link).text
     return response
-#     # else:
-#     #     print(r.status_code)
-#
-#
-#
-def get_page_company(html):           # - функция нахождения ссылки наличия наименований на выбранный бренд автозапчасти
+
+
+
+
+def parse_link_company(html):                                                   # - функция получения ссылки на оригинальный бренд выбранного автомобиля
     soup = BeautifulSoup(html, 'lxml')
     trs = soup.find('table', class_='alt-step-table').find('tbody').find_all('tr')
-    link_brand = []
+    brand = 'KIA / HYUNDAI / MOBIS'
     for tr in trs:
         tds = tr.find_all('td')
         name_comp = tr.find('td', class_='alt-step-table__cell alt-step-table__cell_type_brand').text.strip()
-        # print(name_comp)
-        link_brand.append(name_comp)
-        # print(link_brand)
-        if 'KIA / HYUNDAI / MOBIS' in link_brand:        # - для автоматического выбора только фирмы оригинала нужной марки
+        if name_comp == brand:                                                  # - для автоматического выбора только фирмы оригинала нужной марки
             link_comp = 'https://ryazan.1001z.ru' + tds[2].find('a').get('href')
-            print(link_comp)
             return link_comp
-        else:
-            print('Такой фирмы нет !')
-            # break
-            # print(link_brand.index())
-        # try:
-
-        #      # link_comp = 'https://ryazan.1001z.ru' + tds[2].find('a').get('href')
-        #     # link_brand.append(link_comp)
-        # except:
-        #     link = ''
-        #     print(link)
-
-    # print(link_brand[6])
-
-def get_html_page_pars(link_comp):        # - функция получения html кода страницы парсинга
-    r = requests.get(link_comp)
-    if r.ok:
-        return r.text
-    print(r.status_code)
-
-
-def auto_pars(html):   # - функция парсинга нужных тегов
-    soup = BeautifulSoup(html, 'lxml')
-    trs = soup.find('table', class_='web_ar_datagrid search-results search-results_type_groups').find('tbody').find_all('tr')
-    print(len(trs))
+    print('Такой фирмы нет !')
+    return None
 
 
 
-# def write_csv(data):    #  - функция записи в csv файл
-#     with open('auto_parts.csv', 'a', encoding='utf-8') as file:
-#
-#         fields = ['Производитель, код товара', 'Рейтинг', 'Срок доставки', 'Местоположение товара', 'Наличие', 'Цена']
-#         writer = csv.DictWriter(file, fields, delimiter=';')
-#         writer.writeheader()
-#         writer.writerow()
-#         pass
+def parse_parts(link_comp):                                                      # - функция парсинга наличия вариантов искомой запчасти
+    response_2 = session.get(link_comp).text
+    soup = BeautifulSoup(response_2, 'lxml')
+    trs = soup.find('table', class_='web_ar_datagrid search-results search-results_type_groups').find_all('tr')
+    for tr in trs:
+        tds = tr.find_all('td')
+        try:
+            brand = tds[1].find('span', class_='search-results__info-text').text.strip()       # - Производитель
+            # print(brand)
+        except (IndexError, AttributeError):
+            brand = ''
+        try:
+            number_parts = tds[1].find('div', class_='search-results__article_group').text.strip()       # - Код товара
+            # print(number_parts)
+        except (IndexError, AttributeError):
+            number_parts = ''
+        try:
+            rating = tds[3].find('span', class_='column-val__count').text.strip()       # -  Рейтинг
+            # print(rating)
+        except (IndexError, AttributeError):
+            rating = ''
+        try:
+            delivery_time = tds[4].find('div', class_='search-col__term-wrapper').text.strip()      # - Срок доставки
+            # print(delivery_time)
+        except (IndexError, AttributeError):
+            delivery_time = ''
+        try:
+            parts_location = tr.find('td', class_='search-col search-col__destination_display').text.strip()   # - Местоположение товара
+            # print(parts_location)
+        except (IndexError, AttributeError):
+            parts_location = ''
+        try:
+            availability_parts = tr.find('td', class_='search-col search-col__remains').text.strip()   # - Наличие
+            # print(availability_parts)
+        except (IndexError, AttributeError):
+            availability_parts = ''
+        try:
+            price = tr.find('td', class_='search-col search-col__final_price').text.strip()   # - Цена
+            # print(price)
+        except (IndexError, AttributeError):
+            price = ''
+        data = {'Производитель': brand, 'Код товара': number_parts, 'Рейтинг': rating, 'Срок доставки':  delivery_time,
+                'Местоположение товара': parts_location, 'Наличие': availability_parts, 'Цена': price}
+        return data
+
+
+def writer_csv(data):    #  - функция записи в csv файл
+    with open('auto_parts.csv', 'w', newline='', encoding='utf-8') as file:
+        fields = ['Производитель', 'Код товара', 'Рейтинг', 'Срок доставки', 'Местоположение товара', 'Наличие', 'Цена']
+        writer = csv.DictWriter(file, fieldnames=fields, delimiter=';')
+        writer.writeheader()
+        writer.writerow(data)
+
+
 
 
 
 def main():
     url = 'https://ryazan.1001z.ru/search.html?article={article}&brand=&st=groups&term=0&chk_smode=A&smode=A&sort___search_results_by=final_price&smart_search=1'
-    enter_parts = search_parts(url)
-    avail_parts = get_page_company(enter_parts)
-    url_2 = get_html_page_pars(avail_parts)
-    auto_parts = auto_pars(url_2)
-    # data = write_csv(auto_parts)
+    enter_parts = load_page(url)
+    get_link = parse_link_company(enter_parts)
+    auto_parts = parse_parts(get_link)
+    dic_data = writer_csv(auto_parts)
 
 
 if __name__ == "__main__":
     main()
 
-    # url_2 = 'https://ryazan.1001z.ru/search.html?article=%D0%9C%D0%90%D0%A1%D0%9B%D0%AF%D0%9D%D0%AB%D0%99%D0%A4%D0%98%D0%9B%D0%AC%D0%A2%D0%A0HYUNDAISOLARIS&st=groups&term=0&chk_smode=A&smode=A&sort___search_results_by=final_price&smart_search=1&brand=KIA+%2F+HYUNDAI+%2F+MOBIS'
-    # get_html_pars(url_2)
+
 
 
 
