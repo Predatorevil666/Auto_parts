2fd5ad983a91793181ac7fae97cc99e3db24c323
Alexander Batogov <predatorevil666@mail.ru>
2021-11-09 00:49:42 +0300

    3-я версия парсинга + изменения в txt

--- Auto_parts.py
+++ Auto_parts.py
@@ -1,94 +1,97 @@
-
-
-
 import requests
 import csv
 from bs4 import BeautifulSoup
 
-
-
 # - Масляный фильтр hyundai solaris  (пример для ввода)
 
 session = requests.Session()
-def load_page(url):                                                              # - функция  получения ссылки фирм производителей на запрос конкретной автозапчасти
+
+
+def load_page(url):  # - функция  получения ссылки фирм производителей на запрос конкретной автозапчасти
     article = '+'.join(input('Введите название автозапчасти или ее каталожный номер с маркой автомобиля : ').split())
     link = url.format(article=article)
-    response = session.get(link).text
-    return response
+    response = session.get(link)
+    if response.ok:
+        return response.text
+    print(response.status_code)
 
 
-
-
-def parse_link_company(html):                                                   # - функция получения ссылки на оригинальный бренд выбранного автомобиля
-    soup = BeautifulSoup(html, 'lxml')
+def parse_link_company(enter_parts):  # - функция получения ссылки на оригинальный бренд выбранного автомобиля
+    soup = BeautifulSoup(enter_parts, 'lxml')
     trs = soup.find('table', class_='alt-step-table').find('tbody').find_all('tr')
     brand = 'KIA / HYUNDAI / MOBIS'
     for tr in trs:
         tds = tr.find_all('td')
         name_comp = tr.find('td', class_='alt-step-table__cell alt-step-table__cell_type_brand').text.strip()
-        if name_comp == brand:                                                  # - для автоматического выбора только фирмы оригинала нужной марки
+        if name_comp == brand:  # - для автоматического выбора только фирмы оригинала нужной марки
             link_comp = 'https://ryazan.1001z.ru' + tds[2].find('a').get('href')
             return link_comp
     print('Такой фирмы нет !')
     return None
 
 
-
-def parse_parts(link_comp):                                                      # - функция парсинга наличия вариантов искомой запчасти
-    response_2 = session.get(link_comp).text
+def parse_parts(get_link):  # - функция парсинга наличия вариантов искомой запчасти
+    response_2 = session.get(get_link).text
     soup = BeautifulSoup(response_2, 'lxml')
-    trs = soup.find('table', class_='web_ar_datagrid search-results search-results_type_groups').find_all('tr')
+    trs = soup.select('tr[class^="search-row group"]')
+    data = []
     for tr in trs:
-        tds = tr.find_all('td')
-        try:
-            brand = tds[1].find('span', class_='search-results__info-text').text.strip()       # - Производитель
-            # print(brand)
-        except (IndexError, AttributeError):
-            brand = ''
-        try:
-            number_parts = tds[1].find('div', class_='search-results__article_group').text.strip()       # - Код товара
-            # print(number_parts)
-        except (IndexError, AttributeError):
-            number_parts = ''
-        try:
-            rating = tds[3].find('span', class_='column-val__count').text.strip()       # -  Рейтинг
-            # print(rating)
-        except (IndexError, AttributeError):
-            rating = ''
-        try:
-            delivery_time = tds[4].find('div', class_='search-col__term-wrapper').text.strip()      # - Срок доставки
-            # print(delivery_time)
-        except (IndexError, AttributeError):
-            delivery_time = ''
-        try:
-            parts_location = tr.find('td', class_='search-col search-col__destination_display').text.strip()   # - Местоположение товара
-            # print(parts_location)
-        except (IndexError, AttributeError):
-            parts_location = ''
-        try:
-            availability_parts = tr.find('td', class_='search-col search-col__remains').text.strip()   # - Наличие
-            # print(availability_parts)
-        except (IndexError, AttributeError):
-            availability_parts = ''
-        try:
-            price = tr.find('td', class_='search-col search-col__final_price').text.strip()   # - Цена
-            # print(price)
-        except (IndexError, AttributeError):
-            price = ''
-        data = {'Производитель': brand, 'Код товара': number_parts, 'Рейтинг': rating, 'Срок доставки':  delivery_time,
-                'Местоположение товара': parts_location, 'Наличие': availability_parts, 'Цена': price}
-        return data
+        brand = tr.select('span[class="search-results__info-text"]')  # - Производитель
+        if not brand:
+            continue
+        else:
+            brand = brand[0].text.strip()
+            print(brand)
+        number_parts = tr.select('a[class="search-results__article search-results__article_main"]')  # - Код товара
+        if not number_parts:
+            continue
+        else:
+            number_parts = number_parts[0].text.strip()
+            print(number_parts)
+        rating = tr.select('span[class="column-val__count"]')  # -  Рейтинг
+        if not rating:
+            continue
+        else:
+            rating = rating[0].text.strip()
+            print(rating)
+        delivery_time = tr.select('div[class="search-col__term-wrapper"]')   # - Срок доставки
+        if not delivery_time:
+            continue
+        else:
+            delivery_time = delivery_time[0].text.strip()
+            print(delivery_time)
+        parts_location = tr.select('td[class="search-col search-col__destination_display"]')  # - Местоположение товара
+        if not parts_location:
+            continue
+        else:
+            parts_location = parts_location[0].text.strip()
+            print(parts_location)
+        availability_parts = tr.select('td[class="search-col search-col__remains"]')    # - Наличие
+        if not availability_parts:
+            continue
+        else:
+            availability_parts = availability_parts[0].text.strip()
+            print(availability_parts)
+        price = tr.select('td[class="search-col search-col__final_price"]')   # - Цена
+        if not price:
+            continue
+        else:
+            price = price[0].text.strip()
+            print(price)
+
+        data.append({'Производитель': brand, 'Код товара': number_parts, 'Рейтинг': rating,
+                     'Срок доставки': delivery_time, 'Местоположение товара': parts_location,
+                     'Наличие': availability_parts, 'Цена': price})
+    return data
 
 
-def writer_csv(data):    #  - функция записи в csv файл
+def writer_csv(auto_parts):  # - функция записи в csv файл
     with open('auto_parts.csv', 'w', newline='', encoding='utf-8') as file:
         fields = ['Производитель', 'Код товара', 'Рейтинг', 'Срок доставки', 'Местоположение товара', 'Наличие', 'Цена']
         writer = csv.DictWriter(file, fieldnames=fields, delimiter=';')
         writer.writeheader()
-        writer.writerow(data)
-
-
-
+        for i in auto_parts:
+            writer.writerow(i)
 
 
 def main():
@@ -96,19 +99,8 @@ def main():
     enter_parts = load_page(url)
     get_link = parse_link_company(enter_parts)
     auto_parts = parse_parts(get_link)
-    dic_data = writer_csv(auto_parts)
+    writer_csv(auto_parts)
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-
